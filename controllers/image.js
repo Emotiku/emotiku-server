818625a5d4cc9d06c068a899a2702efc547cca01
sumarsanaadi <sumarsana.adi@gmail.com>
2018-08-15 21:55:51 +0700

    add face emotion detection and rebase

--- controllers/image.js
+++ controllers/image.js
@@ -1,35 +1,127 @@
 const Model = require('../models/image')
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
+const {emotionDetect} = require('../helpers/vision')
 
 const uploadImage = function (req, res) {
-    let emotion = 'test emotion'
-    let user = 'test user'
-    Model.create(
-        {
-            url:req.file.cloudStoragePublicUrl,
-            emotion:emotion
-        }
-    )
-    res.send({
-        status: 200,
-        message: 'Your file is successfully uploaded',
-        link: req.file.cloudStoragePublicUrl
-    })
+    try {
+        const token = req.headers.token
+        let decoded = jwt.verify(token, process.env.JWT_KEY)
+        emotionDetect(req.file.cloudStoragePublicUrl)
+            .then(emotion => {
+                Model.create({
+                    user: decoded.id,
+                    url: req.file.cloudStoragePublicUrl,
+                    emotion: emotion
+                }, (err, result) => {
+                    if (err) {
+                        res.status(500)
+                        res.json({
+                            error: 'error save image url to database'
+                        })
+                    } else {
+                        res.status(200)
+                        res.json({
+                            message: "photo successfully saved to database",
+                            data: result
+                        })
+                    }
+                })
+            })
+            .catch(err => {
+                res.status(500)
+                    res.json({
+                        error: err
+                    })
+            })
+    } catch (err) {
+        res.status(400)
+            .json({
+                message: "token invalid"
+            })
+    }
 }
 
-const deleteImage = function(req,res){
-    Model.remove(
-        {
-            _id:req.params.id
-        }
-    )
-    .then(function () {
-        res.status(200).json({ msg: 'delete image success' })
-    })
-    .catch(function (err) {
-        res.status(500).json({ msg: 'delete image failed' })
-    })
+const deleteImage = function (req, res) {
+    Model.remove({
+            _id: req.params.id
+        })
+        .then(function () {
+            res.status(200).json({
+                message: 'delete image success'
+            })
+        })
+        .catch(function (err) {
+            res.status(500).json({
+                message: 'delete image failed'
+            })
+        })
 
 }
 
+const getImage = (req, res) => {
+    try {
+        const token = req.headers.token
+        let decoded = jwt.verify(token, process.env.JWT_KEY)
+        let query = {
+            user: decoded.id
+        }
+        Model.find(query)
+            .then(img => {
+                res.status(200)
+                res.json({
+                    message: "successfully get photos",
+                    data: img
+                })
+            })
+            .catch(err => {
+                res.status(500)
+                res.json({
+                    error: 'error getting photos'
+                })
+            })
+    } catch (err) {
+        res.status(400)
+            .json({
+                message: "token invalid"
+            })
+    }
+}
 
-module.exports = {uploadImage,deleteImage}
+const getImageById = (req, res) => {
+    try {
+        const token = req.headers.token
+        let decoded = jwt.verify(token, process.env.JWT_KEY)
+        let query = {
+            user: decoded.id,
+            _id: req.params.id
+        }
+        Model.find(query)
+            .then(img => {
+                res.status(200)
+                res.json({
+                    message: "successfully get photo",
+                    data: img
+                })
+            })
+            .catch(err => {
+                res.status(500)
+                res.json({
+                    error: 'error getting photo'
+                })
+            })
+    } catch (err) {
+        res.status(400)
+            .json({
+                message: "token invalid"
+            })
+    }
+}
+
+
+module.exports = {
+    uploadImage,
+    deleteImage,
+    getImage,
+    getImageById
+}
--- helpers/vision.js
+++ helpers/vision.js
@@ -5,22 +5,23 @@ const client = new vision.ImageAnnotatorClient()
 const emotionDetect = function (imgURL) {
   return new Promise(function (resolve, reject) {
     client
-      .faceDetection(img.url)
+      .faceDetection(imgURL)
       .then(results => {
         const faces = results[0].faceAnnotations;
         let numberFaces = faces.length;
-        let emotion = ""
+        let emotion = "Maaf Muka Ga Jelas"
+        console.log(faces)
         faces.forEach((face) => {
-          if (face.joyLikelihood == "VERY_LIKELY" || face.joyLikelihood == "LIKELY") {
+          if (face.joyLikelihood == "VERY_LIKELY" || face.joyLikelihood == "LIKELY" || face.joyLikelihood == "POSSIBLE") {
             emotion = "Joy"
           }
-          if (face.angerLikelihood == "VERY_LIKELY" || face.angerLikelihood == "LIKELY") {
+          if (face.angerLikelihood == "VERY_LIKELY" || face.angerLikelihood == "LIKELY" || face.angerLikelihood == "POSSIBLE") {
             emotion = "Anger"
           }
-          if (face.sorrowLikelihood == "VERY_LIKELY" || face.sorrowLikelihood == "LIKELY") {
+          if (face.sorrowLikelihood == "VERY_LIKELY" || face.sorrowLikelihood == "LIKELY" || face.sorrowLikelihood == "POSSIBLE") {
             emotion = "Sorrow"
           }
-          if (face.surpriseLikelihood == "VERY_LIKELY" || face.surpriseLikelihood == "LIKELY") {
+          if (face.surpriseLikelihood == "VERY_LIKELY" || face.surpriseLikelihood == "LIKELY" || face.surpriseLikelihood == "POSSIBLE") {
             emotion = "Surprise"
           }
         })
@@ -32,4 +33,4 @@ const emotionDetect = function (imgURL) {
   })
 }
 
-module.exports = emotionDetect
+module.exports = {emotionDetect}
