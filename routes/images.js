3892a9366613a4e85b29f40437035a4bee703646
vinesto <oqvinesto@gmail.com>
2018-08-15 19:57:14 +0700

    add image models

--- routes/images.js
+++ routes/images.js
@@ -3,9 +3,11 @@ var router = express.Router();
 var {} = require('../controllers/image')
 
 
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
 
 module.exports = router;
